1653dfe1ff9198a6f2932c288be4e0ac2b62c69d
unknown <catielutz@gmail.com>
2021-03-28 15:15:41 -0500

    completed UFO-level-1 app.js.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,45 @@
 // from data.js
 var ufoData = data;
 
-// select the button and the datetime input
-var button = d3.select("#button");
-var input = d3.select("#datetime");
+// create reference to table, button, and datetime input
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputDate = d3.select("#datetime");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+
+// input data into index.html
+function addData(dataInput) {
+    dataInput.forEach(ufoSighting => {
+        var row = tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSighting[column])
+        );
+    });
+};
+
+addData(ufoData);
+
+// create event listener
+button.on("click", () => {
+    // prevent page from refreshing
+    d3.event.preventDefault();
+
+    var inputValue = inputDate.property("value");
+    var filterDate = ufoData.filter(userDate => userDate.datetime === inputValue);
+
+    // console.log(filterDate);
+
+    tbody.html("");
+    
+    var userInput = {
+        filterDate
+    }
+
+    if(userInput.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+
+        else {
+            tbody.append("tr").append("td").text("Nothing to see here. Move it along, weirdos...");
+    }
+});
